fc7523f7b06888979044fd44466361cdf53dd564
Büşra Merve AYDIN <busramerveaydn@gmail.com>
2021-06-04 15:16:18 +0300

    added

--- Hrms/src/main/java/kodlamaio/Hrms/entities/concretes/ActivationCode.java
+++ Hrms/src/main/java/kodlamaio/Hrms/entities/concretes/ActivationCode.java
@@ -7,11 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import kodlamaio.Hrms.entities.abstracts.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,9 +25,8 @@ public class ActivationCode {
 	@Column(name = "id")
 	private int id;
 	
-	@ManyToOne()
-	@JoinColumn(name = "user_id")
-	private User user;
+	@Column(name = "user_id")
+	private int userId;
 	
 	@Column(name = "activation_code")
 	private String activationCode;
